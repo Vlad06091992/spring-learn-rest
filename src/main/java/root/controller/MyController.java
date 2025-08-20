218f7265ed73686e719bbd9cc4bf7402d116e902
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-08-20 10:39:18 +0500

    пример get запрос для получения сущности(1 штука)

--- src/main/java/root/controller/MyController.java
+++ src/main/java/root/controller/MyController.java
@@ -1,9 +1,7 @@
 package root.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import root.entity.Employee;
 import root.service.EmployeeService;
 import root.service.EmployeeServiceInterface;
@@ -21,5 +19,12 @@ public class MyController {
     public List<Employee> getAllEmployees(){
         List<Employee> employees = this.employeeService.getAllEmployees();
         return employees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable("id") String empId ){
+        List<Employee> employees = this.employeeService.getAllEmployees();
+        Employee employee = this.employeeService.getEmployeeById(empId);
+        return employee;
     }
 }
