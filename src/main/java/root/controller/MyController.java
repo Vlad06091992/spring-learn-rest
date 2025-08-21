8632aa480fdbd6585f9dbfab815064dddb0ddedf
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-08-21 11:28:00 +0500

    пример обработки ошибок

--- src/main/java/root/controller/MyController.java
+++ src/main/java/root/controller/MyController.java
@@ -1,9 +1,12 @@
 package root.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import root.entity.Employee;
-import root.service.EmployeeService;
+import root.exception_handling.EmployeeIncorrectData;
+import root.exception_handling.NoSuchEmployeeException;
 import root.service.EmployeeServiceInterface;
 
 import java.util.List;
@@ -22,9 +25,38 @@ public class MyController {
     }
 
     @GetMapping("/employees/{id}")
-    public Employee getEmployee(@PathVariable("id") String empId ){
-        List<Employee> employees = this.employeeService.getAllEmployees();
+    public Employee getEmployee(@PathVariable("id") String empId ) throws Exception {
         Employee employee = this.employeeService.getEmployeeById(empId);
+
+
+        if("2".equals(empId)){
+            //какой exception - NoSuchEmployeeException
+            throw new Exception("employee with this id not found");
+        }
+
+        if(employee == null){
+            //какой exception - NoSuchEmployeeException
+            throw new NoSuchEmployeeException("employee with this id not found");
+        }
         return employee;
+    }
+
+    @ExceptionHandler
+    //работа с NoSuchEmployeeException
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler
+    //общий Exception
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/main/java/root/exception_handling/EmployeeIncorrectData.java
@@ -0,0 +1,18 @@
+package root.exception_handling;
+
+public class EmployeeIncorrectData {
+
+    public String getInfo() {
+        return info;
+    }
+
+    public String employeeIncorrectData () {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    private String info;
+}
